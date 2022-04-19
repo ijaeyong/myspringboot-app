458c1324608de3df97379e4bd9001b98f274f6d2
jaeyo <jaeyongish@gmail.com>
2022-04-19 16:09:22 +0900

    entity, repository

---
+++ src/main/java/com/basic/myspringboot/entitiy/Account.java
@@ -0,0 +1,18 @@
+package com.basic.myspringboot.entitiy;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Account {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
